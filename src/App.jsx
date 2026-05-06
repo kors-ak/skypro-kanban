3d23e65ba0e0c1c55653d45ab5278f405f76d1b1
kors-ak <alinakorsak86@gmail.com>
2026-05-06 18:58:48 +0300

    feat(popUser): add and remove user info popup triggered by name click

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import PopBrowse from "./components/popups/PopBrowse";
 import PopNewCard from "./components/popups/PopNewCard";
-import PopUser from "./components/popups/PopUser";
 
 function App() {
  return (
   <>
    <div className="wrapper">
-    <PopUser />
     <PopNewCard />
     <PopBrowse />
     <Header />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+import PopUser from "../popups/PopUser";
+
 const Header = () => {
+ const [showPopUser, setShowPopUser] = useState(false);
+
  return (
   <header className="header">
    <div className="container">
@@ -17,21 +22,18 @@ const Header = () => {
       <button className="header__btn-main-new _hover01" id="btnMainNew">
        <a href="#popNewCard">Создать новую задачу</a>
       </button>
-      <a href="#user-set-target" className="header__user _hover02">
+      <a
+       href="#user-set-target"
+       className="header__user _hover02"
+       onClick={(e) => {
+        e.preventDefault();
+        setShowPopUser(!showPopUser);
+       }}
+      >
        Ivan Ivanov
       </a>
-      <div className="header__pop-user-set pop-user-set" id="user-set-target">
-       {/* <a href="">x</a> */}
-       <p className="pop-user-set__name">Ivan Ivanov</p>
-       <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-       <div className="pop-user-set__theme">
-        <p>Темная тема</p>
-        <input type="checkbox" className="checkbox" name="checkbox" />
-       </div>
-       <button type="button" className="_hover03">
-        <a href="#popExit">Выйти</a>
-       </button>
-      </div>
+
+      <PopUser showPopUser={showPopUser} />
      </nav>
     </div>
    </div>
