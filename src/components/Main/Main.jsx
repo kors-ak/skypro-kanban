112ea458469c254ddd217fcf1a40411b5726402c
kors-ak <alinakorsak86@gmail.com>
2026-05-12 15:10:21 +0300

    refactor(Main): migrate from CSS to Styled Components

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,19 +1,20 @@
 import Column from "../Column/Column";
 import { columnsArr } from "../../data";
+import { SMain, SContainer, SBlock, SContent } from "./Main.styled";
 
 const Main = ({ loading }) => {
  return (
-  <main className="main">
-   <div className="container">
-    <div className="main__block">
-     <div className="main__content">
+  <SMain>
+   <SContainer>
+    <SBlock>
+     <SContent>
       {columnsArr.map((column, i) => (
        <Column title={column} loading={loading} key={i}></Column>
       ))}
-     </div>
-    </div>
-   </div>
-  </main>
+     </SContent>
+    </SBlock>
+   </SContainer>
+  </SMain>
  );
 };
 
